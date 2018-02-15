c9a0db9adeeb87ed31e1fbe36f1eb4ab82d36825
Raveen <ravnikator@gmail.com>
2018-02-15 02:28:20 -0800

    number input added

--- main.cpp
+++ main.cpp
@@ -1,42 +1,12 @@
 #include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
-void visualize(int heap[], int place, int depth);
-void add(int heap[], int toAdd);
-
-int main()
-{
-	int heap[100] = {0};
-	//cout << heap[0] << endl;
-	add(heap, 5);
-	//cout << heap[0] << endl;
-	add(heap, 32);
-	//cout << heap[0] << endl;
-	add(heap, 56);
-	//cout << heap[0] << endl;
-	add(heap, 23);
-	//cout << heap[0] << endl;
-	add(heap, 35);
-	//cout << heap[0] << endl;
-	add(heap, 55);
-	//cout << heap[0] << endl;
-	add(heap, 232);
-	//cout << heap[0] << endl;
-	add(heap, 53);
-	//cout << heap[0] << endl;
-	add(heap, 7);
-	//cout << heap[0] << endl;
-	add(heap, 100);
-	//cout << heap[0] << endl;
-	int num = 1/2;
-	//cout << "3/2=" << num << endl;
-	visualize(heap, 1, 0);
-	return 0;
-}
-
 //print out the tree in a simple form
-void visualize(int heap[], int place, int depth)
+void visualize(int heap[], int place = 1, int depth = 0)
 {
 	for(int i = 0; i < depth; i++)
 	{
@@ -56,8 +26,10 @@ void visualize(int heap[], int place, int depth)
 	}
 }
 
+//add numbers to the heap and heapify
 void add(int heap[], int toAdd)
 {
+	//add the number to the end of the heap
 	int added = -1;
 	for(int i = 0; i < 100; i++)
 	{
@@ -69,6 +41,7 @@ void add(int heap[], int toAdd)
 		}
 	}
 	
+	//move the number that was added up until the right spot is found
 	if(added != -1)
 	{
 		int place = added + 1;
@@ -81,7 +54,93 @@ void add(int heap[], int toAdd)
 				heap[above-1] = heap[place-1];
 				heap[place-1] = temp;
 			}
+			else
+			{
+				break;
+			}
 			place = above;
 		}
 	}
+}
+
+int main()
+{
+	int heap[100] = {0};
+	
+	//prompt the user for how numbers will be entered
+	cout << "Would you like to enter numbers via MANUAL input or from FILE? \n";
+	char* input = new char[8];
+	cin >> input;
+	
+	//if the user wants to enter numbers manually
+	if(!strcmp(input, "MANUAL") || !strcmp(input, "manual"))
+	{
+		//add numbers one by one until the user says they are done
+		cout << "Enter integers from 1-1000 one at a time. Enter DONE when you are done adding numbers \n";
+		cin >> input;
+		while(strcmp(input, "DONE") && strcmp(input, "done"))
+		{
+			int toAdd = atoi(input);
+			add(heap, toAdd);
+			cin >> input;
+		}
+		
+		visualize(heap);
+	}
+	
+	//if the user wants to enter numbers from a file
+	else if(!strcmp(input, "FILE") || !strcmp(input, "file"))
+	{
+		char* file = new char[32];
+		char* list = new char[512];
+		
+		//prompt the user for the name of the file
+		cout << "Enter the name of the file.\n";
+		cin.get();
+		cin.get(file, 32);
+		cin.get();
+		
+		//open the file
+		ifstream stream(file);
+		if(stream.is_open())
+		{
+			//extract the text from the file
+			stream.getline(list, 512);
+			stream.close();
+			cout << "list: " << list << endl;
+			//extract the numbers from the text
+			for(int i = 0; i < strlen(list); i++)
+			{
+				if(isdigit(list[i]))
+				{
+					//figure out the length of the integer
+					int start = i;
+					int length = 1;
+					while(list[i+1] != ' ' && i <strlen(list))
+					{
+						length++;
+						i++;
+					}
+					
+					//isolate the integer into a char pointer
+					char* number;
+					for(int j = 0; j < length; j++)
+					{
+						number[j] = list[start+j];
+					}
+					number[length] = '\0';
+					//convert the isolated char pointer into an integer
+					int toAdd = atoi(number);
+					add(heap, toAdd);
+				}
+			}
+		}
+		else
+		{
+			cout << "File not found\n";
+		}
+		
+		visualize(heap);
+	}
+	return 0;
 }
